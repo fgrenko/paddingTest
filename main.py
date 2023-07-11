64098e8764636b12cca17a87ced9958539decc29
acerSwixtXAni <perusicani@gmail.com>
2023-07-11 20:12:27 +0200

    Testing with and without stop signal, pre and post padding

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def regularPadding(fold, X_train, X_val, X_test, y_train, y_val, y_test, padding
         X_train_padded,
         y_train,
         validation_data=(X_val_padded, y_val),
-        epochs=3, # 200 odmah nazad nakon ča skužin da delaju metrike
+        epochs=200, 
         batch_size=32,
         callbacks=[early_stopping_callback],
     )
@@ -106,39 +106,69 @@ def regularPadding(fold, X_train, X_val, X_test, y_train, y_val, y_test, padding
         file.write(f"\nacc: {accuracy}\nmcc: {mcc}\nf1: {f1}\ngm: {gm}")
 
 
-def stopSignal(X_train, X_test, y_train, y_test, padding_type):
+def stopSignal(fold, X_train, X_val, X_test, y_train, y_val, y_test, padding_type):
     # Step 2: Data Preprocessing
     stop_character = -1
-    X_train_stop = [seq + [stop_character] for seq in X_train]
-    X_test_stop = [seq + [stop_character] for seq in X_test]
+    if (padding_type == 'post'):
+        X_train_stop = [seq + [stop_character] for seq in X_train]
+        X_val_stop = [seq + [stop_character] for seq in X_val]
+        X_test_stop = [seq + [stop_character] for seq in X_test]
+    elif (padding_type == 'pre'):
+        X_train_stop = [[stop_character] + seq for seq in X_train]
+        X_val_stop = [[stop_character] + seq for seq in X_val]
+        X_test_stop = [[stop_character] + seq for seq in X_test]
 
-    max_length = max(len(seq) for seq in X_test_stop)
+    # Step 2: Data Preprocessing
+    max_length = max(len(seq) for seq in X_test)
+    # print(max_length)
 
     # Perform padding on the sequences
-    X_train_padded = pad_sequences(X_train_stop, maxlen=max_length, padding='post', value=0)
-    X_test_padded = pad_sequences(X_test_stop, maxlen=max_length, padding='post', value=0)
+    X_train_padded = pad_sequences(X_train_stop, maxlen=max_length, padding=padding_type, value=0.0)
+    X_val_padded = pad_sequences(X_val_stop, maxlen=max_length, padding=padding_type, value=0.0)
+    X_test_padded = pad_sequences(X_test_stop, maxlen=max_length, padding=padding_type, value=0.0)
 
+    # ??? Reshape?
     X_test_padded = np.array(X_test_padded)
+    X_val_padded = np.array(X_val_padded)
     X_train_padded = np.array(X_train_padded)
     y_train = np.array(y_train)
+    y_val = np.array(y_val)
     y_test = np.array(y_test)
 
-    # Step 3: Model Training and Evaluation
-    model = Sequential()
-    model.add(
-        Dense(64, activation='relu', input_dim=max_length))
-    model.add(Dense(1, activation='sigmoid'))
-
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(X_train_padded, y_train, epochs=10, batch_size=32, shuffle=True)
+    model_input = Input(shape=(X_train_padded.shape[1], 1))
+    x = Conv1D(32, 4, padding='same', kernel_initializer='he_normal', name="conv1d_1")(model_input)
+    x = Conv1D(64, 4, padding='same', kernel_initializer='he_normal', name="conv1d_2")(x)
+    x = Bidirectional(LSTM(256, unroll=True, name="bi_lstm"))(x)
+    x = Dropout(0.2, name="dropout")(x)
+    x = Dense(1, activation='sigmoid', name="output_dense")(x)
+    model = Model(inputs=model_input, outputs=x)
+    
+    adam_optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    
+    early_stopping_callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    model.compile(loss="binary_crossentropy", optimizer=adam_optimizer)
+    
+    model.fit(
+        X_train_padded,
+        y_train,
+        validation_data=(X_val_padded, y_val),
+        epochs=200, 
+        batch_size=32,
+        callbacks=[early_stopping_callback],
+    )
 
     # Evaluate the model
     y_pred = model.predict(X_test_padded)
-    y_pred_binary = (y_pred > 0.5).astype(int)
-    accuracy = accuracy_score(y_test, y_pred_binary)
+    y_pred_binary = (y_pred > 0.5).astype(int) # Turn the predictions into 1s and 0s ? Why this crudely
 
-    with open("accuracy.txt", "a") as file:
-        file.write(f"{accuracy}\n")
+    # Metrics
+    accuracy = accuracy_score(y_test, y_pred_binary, normalize=True)
+    mcc = matthews_corrcoef(y_test, y_pred_binary, sample_weight=None)
+    f1 = f1_score(y_test, y_pred_binary, labels=None, pos_label=1, average='binary', sample_weight=None, zero_division='warn')
+    gm = geometric_mean_score(y_test, y_pred_binary.ravel(), labels=None, pos_label=1, average='multiclass', sample_weight=None, correction=0.0)
+    
+    with open((f'stop-signal-metrics-{padding_type}-{fold}.txt'), "a") as file:
+        file.write(f"\nacc: {accuracy}\nmcc: {mcc}\nf1: {f1}\ngm: {gm}")
 
 
 ## legit main
@@ -156,3 +186,5 @@ for i in range(len(xDataSet)):
 
     regularPadding(i, X_train, X_val, X_test, y_train, y_val, y_test, 'post')
     regularPadding(i, X_train, X_val, X_test, y_train, y_val, y_test, 'pre')
+    stopSignal(i, X_train, X_val, X_test, y_train, y_val, y_test, 'post')
+    stopSignal(i, X_train, X_val, X_test, y_train, y_val, y_test, 'pre')
